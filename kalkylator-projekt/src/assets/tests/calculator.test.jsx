7f9599cf19c5428d0cd375eb99af35cf19180031
Fehed Shushan <fehed165@gmail.com>
2025-05-02 00:59:31 +0200

    gör const oo funtion

---
+++ kalkylator-projekt/src/assets/tests/calculator.test.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./Calculator.css";
+
+
+function Calculator() {
+    const [tal1, setTal1] = useState("");
+    const [tal2, setTal2] = useState("");
+    const [operator, setOperator] = useState("+");
+    const [resultat, setResultat] = useState(null);
+
+    
+  const beräkna = () => {
+    const a = Number(tal1);
+    const b = Number(tal2);
